0c2ace52873a85db3a7f707c1d67f7488d41177d
oyh <ouyinghao@oyhdeMacBook-Pro.local>
2018-09-16 09:50:39 +0800

    css-start

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
+
     <h1>First，you should kill a coder</h1>
     <h2>Just a joke</h2>
     <p>Welcome</p>
-    <p>Now make something happen</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
